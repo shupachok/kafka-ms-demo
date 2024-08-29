2638a3d3295b2b2024135e71e9be4388a8afb267
SUPACHOK\shupa <shupachok@gmail.com>
2024-08-29 21:51:24 +0700

    [add]:init project

---
+++ TicketsMicroservice/src/main/java/com/sp/tickets/TicketsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.sp.tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketsMicroserviceApplication.class, args);
+    }
+
+}
