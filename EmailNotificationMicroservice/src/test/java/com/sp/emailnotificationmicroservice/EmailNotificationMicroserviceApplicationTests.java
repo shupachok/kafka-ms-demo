90ade856c6bfc24a0573fa4f8e876df4aabdc21e
SUPACHOK\shupa <shupachok@gmail.com>
2024-09-15 22:19:27 +0700

    [add] kafka consumer microservice

---
+++ EmailNotificationMicroservice/src/test/java/com/sp/emailnotificationmicroservice/EmailNotificationMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sp.emailnotificationmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailNotificationMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
