10897073675669e11034ba20af89c9323700f8d7
SUPACHOK\shupa <shupachok@gmail.com>
2024-09-19 21:12:49 +0700

    [add] recover from deserialization error

--- EmailNotificationMicroservice/src/main/java/com/sp/emailnotificationmicroservice/KafkaConsumerConfiguration.java
+++ EmailNotificationMicroservice/src/main/java/com/sp/emailnotificationmicroservice/KafkaConsumerConfiguration.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
@@ -30,7 +31,10 @@ public class KafkaConsumerConfiguration {
         Map<String, Object> config = new HashMap<>();
         config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        config.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
+
         config.put(JsonDeserializer.TRUSTED_PACKAGES,trustedPackage);
         config.put(ConsumerConfig.GROUP_ID_CONFIG,groupId);
 
